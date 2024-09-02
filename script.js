d9a805562880ca00646fa982fd6d77b942b61897
krishcoderGITHUB <75573226+krishcoderGITHUB@users.noreply.github.com>
2024-09-02 09:54:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,134 @@
+ 
+
+  // fetch('https://script.google.com/macros/s/AKfycbyO9LcesIpSoSnkBAKunpuhYEbzDi0eYaRqQTdXYGcMEfGyfUmCJX8JSLurnyXLfZo0zg/exec?id=akshay')
+  //   .then(response => response.json())
+  //   .then(response => {let loginlist=response.data;
+  //     console.log(loginlist);
+  //   })
+  //   .catch(err => console.error(err));
+
+
+   // Get the snackbar DIV
+   var x = document.getElementById("snackbar");
+   
+  
+function verifyUser() {
+  const IDD = document.getElementById('id').value;
+  const P = document.getElementById('pass').value;
+  if (IDD == "" || P == "") {
+    alert("One of the fields is empty... fill it and then click on login");c
+    
+  } else {
+    fetch('https://script.google.com/macros/s/AKfycbyO9LcesIpSoSnkBAKunpuhYEbzDi0eYaRqQTdXYGcMEfGyfUmCJX8JSLurnyXLfZo0zg/exec?id='+IDD)
+    .then(response => response.json())
+    // .then(res => {const data = res.data;
+    //   console.log(data);
+    // })
+    .then(response => {
+    const users= response.data;
+      console.log(users);
+                           const userId = document.getElementById('id').value;
+    const userPass = document.getElementById('pass').value;
+    const result = document.getElementById('result');
+
+    const user = users.find(user => user.id === userId && user.pass == userPass);
+
+    if (user) {
+        x.textContent = 'Login successful!';
+  // Add the "show" class to DIV
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  document.getElementById("body").style.display = "none";
+  document.getElementById("subtitle").textContent = "Leave Application Portal";
+  document.getElementById("mainpage").style.display = "block";
+  document.getElementById("applicationlist").style.display = "block";
+  document.getElementById("formpage").style.display = "none";
+  document.getElementById("welcome").textContent = "Welcome "+String(IDD);
+ showList(IDD);
+ 
+    } else {
+        // result.textContent = 'Invalid ID or Password.';
+        x.textContent = 'Invalid ID or Password!';
+        // Add the "show" class to DIV
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    }
+    })
+    .catch(err => console.error(err));
+  }
+}
+
+ function showList(IDD){
+
+  fetch('https://script.google.com/macros/s/AKfycbyLTVCMvkv7pi-tfkrEMChCQjSrkHUlnpoo3EpMeF6KT4N0auQWN_-mnF9Nns-R-CAusw/exec?id='+IDD)
+  .then(response => response.json())
+  .then(response => {
+  const leavedata= response.data;
+   
+    const tableBody = document.querySelector('#dataTable tbody');
+
+    leavedata.forEach(item => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${item.id}</td>
+            <td>${item.ai}</td>
+            <td>${item.lp}</td>
+            <td>${item.s}</td>
+        `;
+        tableBody.appendChild(row);
+    });
+
+  }
+  )
+  .catch(err => console.error(err));
+}
+ 
+
+function OpenNewForm() {
+  var add = document.getElementById("addbutton").textContent;
+ 
+   if(add == '+ Apply for Leave'){
+     document.getElementById("useridinput").value = document.getElementById('id').value ;
+     
+   document.getElementById("applicationlist").style.display = "none";
+   document.getElementById("formpage").style.display = "flex";
+   document.getElementById("addbutton").textContent = "Close X";
+   } else {
+     document.getElementById("applicationlist").style.display = "block";
+     document.getElementById("formpage").style.display = "none";
+     document.getElementById("addbutton").textContent = "+ Apply for Leave";
+   
+   }
+  }
+
+  const today = new Date().toISOString().split('T')[0];
+  document.getElementById('single1').setAttribute('min', today);
+  document.getElementById('multiple1').setAttribute('min', today);
+
+  function changeinleavetype() {
+    const leaveType = document.getElementById('leaveType').value;
+    const s1 = document.getElementById("single1");
+    const m1 = document.getElementById("multiple1");
+    const m2 = document.getElementById("multiple2");
+    const s1l =document.getElementById("s1l");
+    const m1l =document.getElementById("m1l");
+    const m2l =document.getElementById("m2l");
+    if (leaveType === 'single') {
+      m1.style.display = "none";
+      m2.style.display = "none";
+      s1.style.display = "block";
+      m1l.style.display = "none";
+      m2l.style.display = "none";
+      s1l.style.display = "block";
+
+    } else {
+      s1.style.display = "none";
+      m1.style.display = "block";
+      m2.style.display = "block";
+      s1l.style.display = "none";
+      m1l.style.display = "block";
+      m2l.style.display = "block";
+    }
+}
+
+
